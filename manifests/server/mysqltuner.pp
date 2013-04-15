982e9936837a476486ecd7b2454be609211efefa
Matthias Saou <matthias@saou.eu>
2013-04-15 12:42:20 +0200

    Use $module_name variable when referring to files in the current module

--- manifests/server/mysqltuner.pp
+++ manifests/server/mysqltuner.pp
@@ -17,6 +17,6 @@ class mysql::server::mysqltuner {
   file { '/usr/bin/mysqltuner':
     ensure  => present,
     mode    => '0550',
-    source  => 'puppet:///modules/mysql/mysqltuner.pl',
+    source  => "puppet:///modules/${module_name}/mysqltuner.pl",
   }
 }
